601ad4dd78c9fba7340e43d333f6b91d4c2c5cf5
qiufengke <shugang17@163.com>
2016-11-17 11:36:26 +0800

    add a new winform project

---
+++ WinFormApp/XmlandDataSet/FormMain.Designer.cs
@@ -0,0 +1,181 @@
+﻿namespace XmlandDataSet
+{
+    partial class FormMain
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FormMain));
+            this.btnToDataSet = new System.Windows.Forms.Button();
+            this.dGV = new System.Windows.Forms.DataGridView();
+            this.btnOpenFile = new System.Windows.Forms.Button();
+            this.txtFile = new System.Windows.Forms.TextBox();
+            this.btnExport = new System.Windows.Forms.Button();
+            this.lblFile = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnFormat = new System.Windows.Forms.Button();
+            this.rtbXml = new System.Windows.Forms.RichTextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dGV)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnToDataSet
+            // 
+            this.btnToDataSet.Location = new System.Drawing.Point(125, 293);
+            this.btnToDataSet.Name = "btnToDataSet";
+            this.btnToDataSet.Size = new System.Drawing.Size(99, 35);
+            this.btnToDataSet.TabIndex = 0;
+            this.btnToDataSet.Text = "↑";
+            this.btnToDataSet.UseVisualStyleBackColor = true;
+            this.btnToDataSet.Click += new System.EventHandler(this.btnToDataSet_Click);
+            // 
+            // dGV
+            // 
+            this.dGV.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.dGV.BackgroundColor = System.Drawing.Color.White;
+            this.dGV.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dGV.Location = new System.Drawing.Point(48, 26);
+            this.dGV.Name = "dGV";
+            this.dGV.RowTemplate.Height = 23;
+            this.dGV.Size = new System.Drawing.Size(696, 203);
+            this.dGV.TabIndex = 1;
+            // 
+            // btnOpenFile
+            // 
+            this.btnOpenFile.Location = new System.Drawing.Point(381, 293);
+            this.btnOpenFile.Name = "btnOpenFile";
+            this.btnOpenFile.Size = new System.Drawing.Size(99, 35);
+            this.btnOpenFile.TabIndex = 0;
+            this.btnOpenFile.Text = "打开文件";
+            this.btnOpenFile.UseVisualStyleBackColor = true;
+            this.btnOpenFile.Click += new System.EventHandler(this.btnOpenFile_Click);
+            // 
+            // txtFile
+            // 
+            this.txtFile.Location = new System.Drawing.Point(190, 249);
+            this.txtFile.Name = "txtFile";
+            this.txtFile.Size = new System.Drawing.Size(425, 21);
+            this.txtFile.TabIndex = 3;
+            // 
+            // btnExport
+            // 
+            this.btnExport.Location = new System.Drawing.Point(516, 293);
+            this.btnExport.Name = "btnExport";
+            this.btnExport.Size = new System.Drawing.Size(99, 35);
+            this.btnExport.TabIndex = 4;
+            this.btnExport.Text = "导出";
+            this.btnExport.UseVisualStyleBackColor = true;
+            this.btnExport.Click += new System.EventHandler(this.btnExport_Click);
+            // 
+            // lblFile
+            // 
+            this.lblFile.AutoSize = true;
+            this.lblFile.Location = new System.Drawing.Point(125, 252);
+            this.lblFile.Name = "lblFile";
+            this.lblFile.Size = new System.Drawing.Size(65, 12);
+            this.lblFile.TabIndex = 5;
+            this.lblFile.Text = "文件路径：";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("宋体", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label1.Location = new System.Drawing.Point(46, 3);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(86, 20);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "DataSet";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(130, 9);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(125, 12);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "(优先从路径加载文件)";
+            // 
+            // btnFormat
+            // 
+            this.btnFormat.Location = new System.Drawing.Point(252, 293);
+            this.btnFormat.Name = "btnFormat";
+            this.btnFormat.Size = new System.Drawing.Size(99, 35);
+            this.btnFormat.TabIndex = 8;
+            this.btnFormat.Text = "格式化xml";
+            this.btnFormat.UseVisualStyleBackColor = true;
+            this.btnFormat.Click += new System.EventHandler(this.btnFormat_Click);
+            // 
+            // rtbXml
+            // 
+            this.rtbXml.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.rtbXml.Location = new System.Drawing.Point(50, 359);
+            this.rtbXml.Name = "rtbXml";
+            this.rtbXml.Size = new System.Drawing.Size(694, 219);
+            this.rtbXml.TabIndex = 9;
+            this.rtbXml.Text = "";
+            // 
+            // FormMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(777, 638);
+            this.Controls.Add(this.rtbXml);
+            this.Controls.Add(this.btnFormat);
+            this.Controls.Add(this.btnOpenFile);
+            this.Controls.Add(this.btnToDataSet);
+            this.Controls.Add(this.btnExport);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.lblFile);
+            this.Controls.Add(this.txtFile);
+            this.Controls.Add(this.dGV);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "FormMain";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "XmlToDataSet 工具";
+            ((System.ComponentModel.ISupportInitialize)(this.dGV)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnToDataSet;
+        private System.Windows.Forms.DataGridView dGV;
+        private System.Windows.Forms.Button btnOpenFile;
+        private System.Windows.Forms.TextBox txtFile;
+        private System.Windows.Forms.Button btnExport;
+        private System.Windows.Forms.Label lblFile;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnFormat;
+        private System.Windows.Forms.RichTextBox rtbXml;
+    }
+}
+
---
+++ WinFormApp/XmlandDataSet/FormMain.cs
@@ -0,0 +1,206 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml;
+using NPOI.HSSF.UserModel;
+
+namespace XmlandDataSet
+{
+    public partial class FormMain : Form
+    {
+        public FormMain()
+        {
+            InitializeComponent();
+        }
+        /// <summary>
+        /// xml to  dataset
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void btnToDataSet_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(rtbXml.Text)) return;
+
+            try
+            {
+                var ds = new DataSet();
+                if (string.IsNullOrEmpty(txtFile.Text))
+                {
+                    var sr = new StringReader(rtbXml.Text);
+                    ds.ReadXml(sr);
+                }
+                else
+                {
+                    ds.ReadXml(txtFile.Text);
+                }
+
+                dGV.DataSource = ds.Tables[ds.Tables.Count - 1];
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+
+        }
+        /// <summary>
+        /// 打开文件
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void btnOpenFile_Click(object sender, EventArgs e)
+        {
+            var oFD = new OpenFileDialog
+            {
+                Title = @"打开文件",
+                ShowHelp = true,
+                Filter = @"xml文件|*.xml|文本文件|*.txt",
+                FilterIndex = 1,
+                RestoreDirectory = false,
+                InitialDirectory = "c:\\",
+                Multiselect = false
+            };
+
+            if (oFD.ShowDialog() != DialogResult.OK) return;
+            txtFile.Text = oFD.FileName;
+
+            try
+            {
+                var ds = new DataSet();
+                //从路径读取文件
+                ds.ReadXml(txtFile.Text);
+                dGV.DataSource = ds.Tables[ds.Tables.Count - 1];
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            finally
+            {
+                var sr = new StreamReader(txtFile.Text, oFD.FileName.EndsWith(".txt") ? Encoding.Default : Encoding.UTF8);
+                rtbXml.Text = sr.ReadToEnd();
+            }
+        }
+        /// <summary>
+        /// 导出按钮
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void btnExport_Click(object sender, EventArgs e)
+        {
+            var ds = dGV.DataSource as DataTable;
+            if (ds == null) return;
+
+            var sfd = new SaveFileDialog
+            {
+                Filter = @"xls files(*.xls)|*.xls",
+                FileName = ds.TableName,
+                AddExtension = true,
+                RestoreDirectory = true
+            };
+            if (sfd.ShowDialog() == DialogResult.OK)
+            {
+                ExportToExcel(ds, sfd);
+            }
+
+        }
+        /// <summary>
+        /// npoi 导出
+        /// </summary>
+        /// <param name="ds"></param>
+        /// <param name="sfd"></param>
+        private static void ExportToExcel(DataTable ds, SaveFileDialog sfd)
+        {
+            var rowCount = ds.Rows.Count;
+            var columnCount = ds.Columns.Count;
+            var fileName = sfd.FileName;
+
+            if (columnCount > 0 && rowCount > 0)
+            {
+                var wb = new HSSFWorkbook();
+                var sheet = wb.CreateSheet("表1");
+                var header = sheet.CreateRow(0);
+                for (var i = 0; i < columnCount; i++)
+                {
+                    header.CreateCell(i).SetCellValue(ds.Columns[i].ColumnName);
+                }
+                for (var i = 0; i < rowCount; i++)
+                {
+                    var tr = sheet.CreateRow(i + 1);
+                    for (var j = 0; j < columnCount; j++)
+                    {
+                        var cellValue = 0;
+                        if (int.TryParse(ds.Rows[i][j].ToString(), out cellValue))
+                        {
+                            tr.CreateCell(j).SetCellValue(cellValue);
+                        }
+                        else
+                        {
+                            tr.CreateCell(j).SetCellValue(ds.Rows[i][j].ToString());
+                        }
+                    }
+                }
+
+                //var path = System.AppDomain.CurrentDomain.BaseDirectory;
+                //var fileName = path + ds.TableName + ".xls";
+                using (var file = new FileStream(fileName, FileMode.OpenOrCreate))
+                {
+                    wb.Write(file);
+                    file.Close();
+                }
+            }
+        }
+
+        private static string FormatXml(string xml)
+        {
+            var xd = new XmlDocument();
+            xd.LoadXml(xml);
+            var sw = new StringWriter();
+            XmlTextWriter xtw = null;
+            try
+            {
+                xtw = new XmlTextWriter(sw)
+                {
+                    Formatting = Formatting.Indented,
+                    Indentation = 2
+                };
+                xd.WriteTo(xtw);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+                return xml;
+            }
+            finally
+            {
+                if (xtw != null) xtw.Close();
+            }
+            return sw.ToString();
+        }
+        /// <summary>
+        /// 格式化xml文件
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void btnFormat_Click(object sender, EventArgs e)
+        {
+            rtbXml.Text = FormatXml(rtbXml.Text);
+        }
+        //private string GetXmlContent(string xml)
+        //{
+        //    var xDoc = new XmlDocument();
+        //    xDoc.LoadXml(xml);
+        //    var rows = xDoc.SelectNodes("/ResultData/Row");
+        //    if (rows.Count == 0) return "";
+        //    var result = rows.Cast<XmlNode>().Aggregate("", (current, item) => current + item.OuterXml);
+        //    return result;
+        //}
+    }
+}
